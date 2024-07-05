a0855d20d0eacb01045ca67741b845fa8f5e3cf5
joaoeduardo2701 <duducardoso2701@gmail.com>
2024-07-05 15:08:23 -0300

    alterações na tela principal

--- TelaPrincipalWinApp/TelaPrincipalForm.cs
+++ TelaPrincipalWinApp/TelaPrincipalForm.cs
@@ -9,10 +9,8 @@ namespace TelaPrincipalWinApp
 
         private void produtoToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            btnAdicionar.ToolTipText = "Cadastrar Produto";
             btnEditar.ToolTipText = "Editar Produto";
             btnExcluir.ToolTipText = "Excluir Produto";
         }
     }
 }
-    
